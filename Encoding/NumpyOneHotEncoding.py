3a6f639103bf94e2a1a9b30b687b4432858f2d1a
Unknown <eng_mohamed_magdy@hotmail.com>
2017-04-04 21:26:09 +0200

    Finish One Hot Encoding

--- Encoding/NumpyOneHotEncoding.py
+++ Encoding/NumpyOneHotEncoding.py
@@ -1,4 +1,5 @@
 import numpy as np
+import MySQLdb
 
 def one_hot_encode(x, n_classes):
     """
@@ -6,18 +7,37 @@ def one_hot_encode(x, n_classes):
     : x: List of sample Labels
     : return: Numpy array of one-hot encoded labels
      """
-    return np.eye(n_classes)[x]
+    return np.eye(n_classes)
 
-def main():
-    newList = [];
-    for x in range (0,1000):
-        newList.append(x);
-        print newList[x];
-    list = [0,1,2,3,4,3,2,1,0]
-    n_classes = 1000
-    one_hot_list = one_hot_encode(newList, n_classes)
-    print(one_hot_list)
+db = MySQLdb.connect(host="127.0.0.1",  # your host, usually localhost
+                         user="root",  # your username
+                         passwd="Islammega88",  # your password
+                         db="MSTDB",  # name of the data base
+                         use_unicode=True,
+                         charset="utf8",
+                         init_command='SET NAMES UTF8')
 
-if __name__ == "__main__":
-    main()
+cur = db.cursor()
+listOfUnDiacritizedCharacter = "SELECT Distinct undiacritizedCharacter FROM ListOfPurifiedCharacters"
+listOfDiacritizedCharacter = "SELECT diacritizedCharacter FROM ListOfPurifiedCharacters"
+
+cur.execute(listOfUnDiacritizedCharacter);
+rowsUnDiacritizedCharacter = cur.fetchall();
+
+cur.execute(listOfDiacritizedCharacter);
+rowsOfDiacritizedCharacter = cur.fetchall();
+
+db.commit()
+
+db.close()
+
+n_UnDiacritizedClasses = len(rowsUnDiacritizedCharacter)
+n_DiacritizedClasses = len(rowsOfDiacritizedCharacter)
+
+one_hot_list_ForUnDiacritizedCharacters = one_hot_encode(listOfUnDiacritizedCharacter, n_UnDiacritizedClasses)
+one_hot_list_ForDiacritizedCharacters = one_hot_encode(listOfDiacritizedCharacter, n_DiacritizedClasses)
+
+print(one_hot_list_ForDiacritizedCharacters)
+print(one_hot_list_ForUnDiacritizedCharacters)
+
 
