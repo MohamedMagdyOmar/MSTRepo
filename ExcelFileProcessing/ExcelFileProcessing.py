35a3a013ccf138e4aa24df61ea0952f55f2b23a5
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2017-06-27 14:54:26 +0200

    correct errors in excel file

--- ExcelFileProcessing/ExcelFileProcessing.py
+++ ExcelFileProcessing/ExcelFileProcessing.py
@@ -6,6 +6,8 @@ import MySQLdb
 import MySQLdb.cursors
 import os
 import glob
+from xlrd import open_workbook
+from xlutils.copy import copy
 
 rnn_output_for_one_seq = []
 neurons_locations_with_highest_output_for_a_seq = []
@@ -15,13 +17,21 @@ list_of_expected_letters = []
 
 list_of_actual_letters_errors = []
 list_of_expected_letters_errors = []
-number_of_diacritization_errors = 0
-sentence_number = 0
-col = -1
-col_for_error = -1
+current_sentence = []
 
+total_error = 0
+sentence_number = 0
+
+first_time = True
 path = 'D:\CurrenntRepo\CurrenntVS\CURRENNT\ArabicDiacritizationExample'
+diacritization_error_excel_file_path = "D:\CurrenntRepo\CurrenntVS\CURRENNT\ArabicDiacritizationExample\Errors" \
+                                       "\Book1.xls "
 extension = 'csv'
+
+row_of_errors_excel_file = 0
+
+workbook = xlsxwriter.Workbook(diacritization_error_excel_file_path)
+worksheet = workbook.add_worksheet()
 
 
 def connect_to_db():
@@ -47,6 +57,18 @@ def get_number_of_first_testing_sentence():
     sentence_number = int((sentence_number[0])[0])
 
 
+def get_sentence_from_db():
+    connect_to_db()
+    selected_sentence_query = "select Word from parseddocument where LetterType='testing' and SentenceNumber = " + \
+                              str(sentence_number)
+
+    cur.execute(selected_sentence_query)
+
+    global current_sentence
+    current_sentence = cur.fetchall()
+    current_sentence = sorted(set(current_sentence), key=lambda x: current_sentence.index(x))
+
+
 def read_csv_file(filename):
     path_of_file = 'D:\CurrenntRepo\CurrenntVS\CURRENNT\ArabicDiacritizationExample\\' + filename
 
@@ -62,13 +84,13 @@ def read_csv_file(filename):
 
 def get_neurons_numbers_with_highest_output_value():
     global rnn_output_for_one_seq
+    global neurons_locations_with_highest_output_for_a_seq
     neurons_locations_with_highest_output_for_a_seq = []
     for row in rnn_output_for_one_seq:
         neurons_locations_with_highest_output_for_a_seq.append(row.index(max(row)))
 
 
 def get_all_letters_from_db():
-
     list_of_all_diacritized_letters_query = "select DiacritizedCharacter from diaconehotencoding"
 
     cur.execute(list_of_all_diacritized_letters_query)
@@ -80,12 +102,13 @@ def get_all_letters_from_db():
 def get_actual_letters():
     global list_of_actual_letters
     list_of_actual_letters = []
+
+    global neurons_locations_with_highest_output_for_a_seq
     for neuron_location in neurons_locations_with_highest_output_for_a_seq:
         list_of_actual_letters.append(list_of_all_diacritized_letters[neuron_location - 1])
 
 
 def get_expected_letters():
-
     global sentence_number
     list_of_expected_diacritized_letters_query = "select DiacritizedCharacter from parseddocument where " \
                                                  "LetterType='testing' and SentenceNumber = " + str(sentence_number)
@@ -98,64 +121,94 @@ def get_expected_letters():
 
 
 def write_data_into_excel_file(path_of_file):
-    workbook = xlsxwriter.Workbook(path_of_file)
-    worksheet = workbook.add_worksheet()
 
-    row = 0
+    global first_time
+    if first_time:
+        workbook = xlsxwriter.Workbook(path_of_file)
+        worksheet = workbook.add_worksheet()
+    else:
+        w = copy(path_of_file)
+        worksheet = w.get_sheet(0)
 
-    global col
-    col += 1
+    global row_of_letters_excel_file
+    row_of_letters_excel_file += 1
 
-    for letter in list_of_expected_letters:
-        worksheet.write(row, col, letter[0])
-        row += 1
+    column = 0
 
-    row = 0
-    col += 1
+    for expected_letter, actual_letter in zip(list_of_expected_letters, list_of_actual_letters):
+        worksheet.write(row_of_letters_excel_file, column, expected_letter[0])
+        column = 1
+        worksheet.write(row_of_letters_excel_file, column, actual_letter[0])
+        row_of_letters_excel_file += 1
+        column = 0
 
-    for letter in list_of_actual_letters:
-        worksheet.write(row, col, letter[0])
-        row += 1
+    row_of_letters_excel_file += 1
 
-    col += 1
+    if not first_time:
+        w.save(path_of_file)
+
     workbook.close()
 
 
 def get_diacritization_error():
-    global number_of_diacritization_errors
+    global list_of_actual_letters_errors
+    global list_of_expected_letters_errors
+    global total_error
+
+    number_of_diacritization_errors = 0
     counter = 0
+    letter_location = 0
+    list_of_error_locations = []
     for letter in list_of_actual_letters:
+        letter_location += 1
         if letter[0] != (list_of_expected_letters[counter])[0]:
             list_of_actual_letters_errors.append(letter)
             list_of_expected_letters_errors.append(list_of_expected_letters[counter])
+            list_of_error_locations.append(letter_location)
             number_of_diacritization_errors += 1
 
         counter += 1
 
+    total_error += number_of_diacritization_errors
+
     print number_of_diacritization_errors
+    print 'total error: ', total_error
 
-    workbook = xlsxwriter.Workbook('D:\CurrenntRepo\CurrenntVS\CURRENNT\ArabicDiacritizationExample\Errors'
-                                   '\diacritizationErrors.xlsx')
-    worksheet = workbook.add_worksheet()
+    wb = open_workbook(diacritization_error_excel_file_path)
+    w = copy(wb)
+    worksheet = w.get_sheet(0)
 
-    row = 0
-    global col_for_error
-    col_for_error += 1
+    global row_of_errors_excel_file
+    row_of_errors_excel_file += 1
 
-    for letter in list_of_actual_letters_errors:
-        worksheet.write(row, col_for_error, letter[0])
-        row += 1
+    column = 0
+    i = 0
+    for expected_letter, actual_letter in zip(list_of_expected_letters_errors, list_of_actual_letters_errors):
+        worksheet.write(row_of_errors_excel_file, column, expected_letter[0])
 
-    row = 0
-    col_for_error += 1
+        column = 1
+        worksheet.write(row_of_errors_excel_file, column, actual_letter[0])
 
-    for letter in list_of_expected_letters_errors:
-        worksheet.write(row, col_for_error, letter[0])
-        row += 1
+        column = 2
+        worksheet.write(row_of_errors_excel_file, column, list_of_error_locations[i])
 
-    col_for_error += 1
+        i += 1
+        row_of_errors_excel_file += 1
+        column = 0
+
+    all_sentence = ''
+    for each_word in current_sentence:
+        all_sentence += each_word[0] + ' '
+    worksheet.write(row_of_errors_excel_file, column, all_sentence)
+
+    row_of_errors_excel_file += 1
+
+    w.save(diacritization_error_excel_file_path)
+
     workbook.close()
 
+    list_of_actual_letters_errors = []
+    list_of_expected_letters_errors = []
 
 if __name__ == "__main__":
     connect_to_db()
@@ -165,12 +218,13 @@ if __name__ == "__main__":
     result = [i for i in glob.glob('*.{}'.format(extension))]
 
     for file_name in result:
+        get_sentence_from_db()
         read_csv_file(file_name)
         get_neurons_numbers_with_highest_output_value()
         connect_to_db()
         get_all_letters_from_db()
         get_actual_letters()
         get_expected_letters()
-        write_data_into_excel_file('D:\CurrenntRepo\CurrenntVS\CURRENNT\ArabicDiacritizationExample\Book1.xlsx')
+        # write_data_into_excel_file('D:\CurrenntRepo\CurrenntVS\CURRENNT\ArabicDiacritizationExample\Book1.xlsx')
         get_diacritization_error()
         sentence_number += 1
